af2edcdb728ca24af0b5fadec7344554d17e6619
yann roth <yann.roth0@gmail.com>
2025-08-21 11:50:46 +0200

    crude webui with no funktionality

--- src/main.py
+++ src/main.py
@@ -11,12 +11,23 @@ import math
 import csv
 from Keithley2000 import Keithley2000
 from measurement_ui import KeithleyCustomTkinterGUI
+import multiprocessing
+from measurement_web_ui import app as flask_app
+
+
+def start_flask():
+    flask_app.run(debug=True, use_reloader=False)
 
 
 if __name__ == "__main__":
+    flask_process = multiprocessing.Process(target=start_flask)
+    flask_process.start()
+
     root = tk.Tk()
     root.protocol(
         "WM_DELETE_WINDOW", root.quit
     )  # Ensure the application closes properly, when the window is closed
     app = KeithleyCustomTkinterGUI(root)
     root.mainloop()
+
+    flask_process.terminate()
